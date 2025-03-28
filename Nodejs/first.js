be4bf3ba50d4d56080a11e011fcea1d97936c304
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-28 18:18:26 +0530

    Add files via upload

--- Nodejs/first.js
+++ Nodejs/first.js
@@ -1,2 +1,7 @@
-console.log("hello world");  
+const calculate = require('./calculator');
+
+
+
+
+console.log("this is calculate your vale" , calculate.add (10,23) +"and"+ calculate.sub(33574357454,77))
 
